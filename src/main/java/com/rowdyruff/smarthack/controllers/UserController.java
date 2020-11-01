6f22ee95443dc8d87b7d70d890d3c0772609599d
Alex Jilavu <alexjilavu17@gmail.com>
2020-11-01 06:45:32 +0200

    update create user si drop la db

--- src/main/java/com/rowdyruff/smarthack/controllers/UserController.java
+++ src/main/java/com/rowdyruff/smarthack/controllers/UserController.java
@@ -109,7 +109,7 @@ public class UserController extends GenericController<User> {
 	public ResponseEntity<?> createUser(@RequestBody CreateUserRequest req) {
 		try {
 			Institution institution = null;
-			if (req.getRole() != null && (req.getRole().equals(Role.ROLE_INSTITUTION_ADMIN.name()) || req.getRole().equals(Role.ROLE_INSTITUTION_ADMIN.name())))
+			if (req.getRole() != null && (req.getRole().equals(Role.ROLE_INSTITUTION_ADMIN.name()) || req.getRole().equals(Role.ROLE_INSTITUTION_USER.name())))
 				institution = institutionService.getItem(req.getInstitutionId());
 			
 			User user = new User(req, institution);
